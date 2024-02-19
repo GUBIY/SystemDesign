4a008e5f28bcdbddd172a8a3b3f53733d7b2f4f7
snowflake-Zhao <763585773@qq.com>
2024-02-19 16:33:08 +0800

    https://zrzr20220926114535774.pingcode.com/pjm/items/65d311c404ae6f86b7c0603c? #XTS-20 pseduRequestHandler
    提交了基础功能以及ab测试脚本

--- sys_design_proj/rate-limiter/src/main/java/com/example/ratelimiter/HandlerController.java
+++ sys_design_proj/rate-limiter/src/main/java/com/example/ratelimiter/HandlerController.java
@@ -7,17 +7,41 @@ import java.io.*;
 
 @RestController
 public class HandlerController {
+  //////////////////////////////////
+  // Testing Apache Bench
+  // ab -n 100 -c 100 http://localhost:8090/monolithicdosth
+  //////////////////////////////////
+  public static final String ERR_TOO_MANY_REQUESTS = "Too many requests";
+  public static final String LOG_EXCEED_REQUEST_LIMIT = "Reaching the limit of the request:{%d}";
+  public static int processingReqCount = 0;
+  public static int processingReqLimit = 10;
+  public static final Object syncObj = new Object();
 
-  @RequestMapping
   public String handler() throws Exception {
     throw new Exception("test");
-
   }
 
-  @RequestMapping(value = "/dosomething", method = RequestMethod.GET)
+
+  @RequestMapping(value = "/monolithicdosth", method = RequestMethod.GET)
   public void dosomething(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setStatus(429);
-    response.getOutputStream().write("Too many requests".getBytes());
+    synchronized (syncObj) {
+      processingReqCount++;
+      try {
+        if (processingReqCount >= processingReqLimit) {
+          System.out.println(String.format(LOG_EXCEED_REQUEST_LIMIT, processingReqCount));
+          response.setStatus(429);
+          response.getOutputStream().write(ERR_TOO_MANY_REQUESTS.getBytes());
+          return;
+        }
+        // TODO:想想我们怎么搞一下，这个处理request的逻辑
+        processRequest();
+      } finally {
+        processingReqLimit--;
+      }
+    }
   }
 
+  private void processRequest() {
+
+  }
 }
